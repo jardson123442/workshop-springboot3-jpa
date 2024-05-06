3a4eb31411c5ce08314e6dfbb258a564955c76f1
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-05-06 15:33:05 -0300

    User entity and resource

---
+++ src/main/java/com/jardsonproject/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.jardsonproject.course.resources;
+
+import com.jardsonproject.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1, "Maria", "maria2gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
