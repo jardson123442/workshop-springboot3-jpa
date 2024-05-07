2d4bbb63384402b353bf06de7a122422cb1c706c
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-05-07 17:09:26 -0300

    Exception handling - delete

--- src/main/java/com/jardsonproject/course/services/UserService.java
+++ src/main/java/com/jardsonproject/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.jardsonproject.course.services;
 
 import com.jardsonproject.course.entities.User;
 import com.jardsonproject.course.repositories.UserRepository;
+import com.jardsonproject.course.services.exceptions.DatabaseException;
 import com.jardsonproject.course.services.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,7 +24,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
@@ -29,11 +32,17 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
+        User entity = repository.getOne(id);
         updateData(entity, obj);
         return repository.save(entity);
     }
